24b7de2d6c8d3b5577ff1534638406fe15872b36
SantiagoGirbau <santiagogirbau@gmail.com>
2025-10-06 19:36:01 -0300

    Acceso a datos

--- Models/AccesoADatos.cs
+++ Models/AccesoADatos.cs
@@ -1,4 +1,30 @@
-class AccesoADatos
+
+using System.Text.Json;
+public class AccesoADatos
 {
-    
+    private string? rutaDeAcceso;
+
+    public List<Tarea> leerDatos()
+    {
+        var textoTareas = File.ReadAllText(rutaDeAcceso);
+        var tareas = JsonSerializer.Deserialize<List<Tarea>>(textoTareas);
+        return tareas;
+    }
+
+    public bool guardarDatos(List<Tarea> tareas)
+    {
+
+        string jsonString = JsonSerializer.Serialize(tareas, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText("Pedidos.json", jsonString);
+
+        if (jsonString != null)
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
+
+    }
+
 }
